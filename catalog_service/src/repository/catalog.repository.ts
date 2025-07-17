33e8627710950099b8aa8ae8edeeea7da4c433ba
ganglinwu <ganglin.wu@gmail.com>
2025-07-17 13:39:27 +0800

    feat: implemented repository methods
    - using prisma ORM
    - no unit tests

--- catalog_service/src/repository/catalog.repository.ts
+++ catalog_service/src/repository/catalog.repository.ts
@@ -1,20 +1,57 @@
+import { PrismaClient } from "../generated/prisma";
 import { ICatalogRepository } from "../interface/catalogRepository.interface";
 import { Product } from "../models/products.model";
 
 export class CatalogRepository implements ICatalogRepository {
+  _prisma: PrismaClient;
+
+  constructor() {
+    this._prisma = new PrismaClient();
+  }
+
   create(data: Product): Promise<Product> {
-    throw new Error("Method not implemented.");
+    return this._prisma.product.create({
+      data: data,
+    });
   }
   update(data: Product): Promise<Product> {
-    throw new Error("Method not implemented.");
+    return this._prisma.product.update({
+      where: {
+        id: data.id,
+      },
+      data: data,
+    });
   }
-  delete(id: number): Promise<number> {
-    throw new Error("Method not implemented.");
+  async delete(id: number): Promise<number> {
+    try {
+      const deletedProduct = await this._prisma.product.delete({
+        where: {
+          id: id,
+        },
+      });
+      console.log("Product deleted successfully:", deletedProduct);
+      return Promise.resolve(id);
+    } catch (error) {
+      console.error("Failed to delete product:", error);
+      throw error;
+    }
   }
-  find(): Promise<Product[]> {
-    throw new Error("Method not implemented.");
+  find(limit: number, offset: number): Promise<Product[]> {
+    return this._prisma.product.findMany({
+      take: limit,
+      skip: offset,
+    });
   }
   findOne(id: number): Promise<Product> {
-    throw new Error("Method not implemented.");
+    const product = this._prisma.product.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    if (product !== null) {
+      return product as Promise<Product>;
+    } else {
+      throw new Error(`product with specified id ${id} not found`);
+    }
   }
 }
