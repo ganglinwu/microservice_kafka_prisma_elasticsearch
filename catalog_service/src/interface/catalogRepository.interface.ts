808fc67849421b06f3ba08b0dbf7946d3a98f6c8
ganglinwu <ganglin.wu@gmail.com>
2025-07-24 13:05:24 +0800

    refactor: migrate Product model from number to UUID string IDs
    - Update Product model to use optional string ID instead of number
    - Update ICatalogRepository interface methods to accept string IDs
    - Change delete and findOne method signatures for UUID compatibility

--- catalog_service/src/interface/catalogRepository.interface.ts
+++ catalog_service/src/interface/catalogRepository.interface.ts
@@ -3,7 +3,7 @@ import { Product } from "../models/products.model";
 export interface ICatalogRepository {
   create(data: Product): Promise<Product>;
   update(data: Product): Promise<Product>;
-  delete(id: number): Promise<number>;
+  delete(id: string): Promise<string>;
   find(limit: number, offset: number): Promise<Product[]>;
-  findOne(id: number): Promise<Product>;
+  findOne(id: string): Promise<Product>;
 }
